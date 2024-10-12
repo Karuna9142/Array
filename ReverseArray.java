cbcc8d862651849e92b51297ce971a30f9440e85
Karuna Kumari <mishrakaruna9142@gmail.com>
2024-10-12 23:55:22 +0530

    Add files via upload

--- ReverseArray.java
+++ ReverseArray.java
@@ -4,38 +4,33 @@ import java.util.Scanner;
 
 public class ReverseArray {
 	
-	static void rev_arr(int arr[])
+	public static void rev_arr(int arr[])
 	{
-		int i = arr.length-1;
-		
-		while(i>=0)
-		{
-			System.out.println(arr[i]);
-			i--;
-		}
-		/*
+		System.out.println("Reversed array is:-  ");
 		for(int i = arr.length-1; i>=0; i--)
 		{
 			System.out.println(arr[i]);
 		}
-		*/
 	}
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the size of the array:- ");
-		
-		int n = sc.nextInt();
-		int arr[] = new int[n];
-		
-		System.out.println("Enter the element of the array");
-		for(int i = 0; i<arr.length; i++)
-		{
-			arr[i] = sc.nextInt();
-		}
-		rev_arr(arr);
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" enter the element");
+        
+        int arr[] = new int[5];
+        
+        for(int i = 0; i<arr.length; i++)
+        {
+        	arr[i] = sc.nextInt();
+        }
+        
+        rev_arr(arr);
+        
+        
+        
+        
 	}
 
 }

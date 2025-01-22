fbd580d598170f247a9e623708216ca82e8a2294
Karuna Kumari <mishrakaruna9142@gmail.com>
2025-01-22 23:18:14 +0530

    Add files via upload

--- ArrayEx.java
+++ ArrayEx.java
@@ -1,43 +1,26 @@
 package com.array;
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class ArrayEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		/*
-		//array of primitives
-		int arr[] = new int[5];
 		
-		System.out.println("Enter the element");
-		for(int i = 0; i<arr.length; i++)
-		{
-			arr[i] = sc.nextInt();
-		}
-		///for each loop
-		/*
-		for(int num:arr)
-		{
-			System.out.println(num); //here num represent the element of the array
-		}
-       
-		System.out.println("Elemet of the array are");
-		System.out.println(Arrays.toString(arr));//other way of printing the element
-		*/
+		System.out.println("Enter the size of the array");
+		int size = sc.nextInt();
+		
+		int arr[] = new int[size];
 		System.out.println("Enter the element of the array");
-		String arr[] = new String[5];
 		for(int i = 0; i<arr.length; i++)
-		{
-			arr[i] = sc.nextLine();
-		}
-		System.out.println(Arrays.toString(arr));
-		arr[2] = "Karuna";
-		arr[3] = "Abhishek";
+			arr[i] = sc.nextInt();
 		
-		System.out.println(Arrays.toString(arr));
+		System.out.println("Element of the array are");
+		//for(int i = 0; i<arr.length; i++)
+			//System.out.println(arr[i]);
 		
+		for(int i:arr)
+			System.out.println(i);
 	}
 
 }
